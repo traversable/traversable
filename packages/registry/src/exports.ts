7226e3d66262be9df52523e5168a5f84a2e668b0
Andrew Jarrett <ahrjarrett@gmail.com>
2024-12-06 11:55:54 -0600

    ✨(registry): adds new module: `registry.newtypes`, `registry.utf-16`

--- packages/registry/src/exports.ts
+++ packages/registry/src/exports.ts
@@ -1,27 +1,31 @@
 import type { newtype } from "any-ts"
 
+export type * from "./types.js"
+export type { emoji, gitmoji, jsdoc, math } from "./utf-16.js"
+export type { integer, number } from "./newtypes.js"
+export type { string } from "./newtypes.js"
+export type { SymbolRegistry } from "./registry.js"
+
 export * from "./version.js"
 export * as Invariant from "./error.js"
 export { PATTERN } from "./pattern.js"
 export { URI, symbol } from "./symbol.js"
-export type { SymbolRegistry } from "./registry.js"
-export type * from "./types.js"
 export { createRegistry, createRegistry as create, register } from "./registry.js"
 
 export type Register<
   T,
-  U extends { [URI in keyof T & string]: Known<URI, T[URI] & symbol> } = {
+  _ extends { [URI in keyof T & string]: Known<URI, T[URI] & symbol> } = {
     [URI in keyof T & string]: Known<URI, T[URI] & symbol>
   },
-> = { [K in keyof U]: U[K] }
+> = { [K in keyof _]: _[K] }
 
 /**
  * ## {@link symbol_new `symbol.new`}
  *
  * TODO: add usage docs
  */
-export interface Known<URI extends string, Symbol extends symbol>
+export interface Known<URI extends string, Sym extends symbol>
   extends newtype<globalThis.Omit<Symbol, "valueOf" | "toString">> {
-  valueOf(): Symbol
+  valueOf(): Sym
   toString(): `Symbol(${URI})`
 }
