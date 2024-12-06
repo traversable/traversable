ce307b6770136f7e423bd20a5173fd489b76beda
Andrew Jarrett <ahrjarrett@gmail.com>
2024-12-05 21:51:02 -0600

    🧹(build): optimizes `build:clean` script

--- bin/clean-build.ts
+++ bin/clean-build.ts
@@ -1,16 +1,13 @@
 #!/usr/bin/env pnpm dlx tsx
 import { Print, rimraf } from "./util.js"
 import { BUILD_ARTIFACTS, EMOJI, GRAPH } from "./constants.js"
+import { flow } from "effect/Function"
 
 const log = (): void => 
   void Print(Print.task(`Cleaning build artifacts... ${EMOJI.WELL}`))
 
 const cleanBuild = (): void =>
-  void GRAPH.forEach(
-    node => BUILD_ARTIFACTS
-      .map(path => `${node}/${path}`)
-      .forEach(rimraf),
-  )
+  void GRAPH.forEach(node => BUILD_ARTIFACTS.forEach(flow(path => `${node}/${path}`, rimraf)))
 
 const main = (): void => (
   void log(),
