54c6391ac5fc0c58938c30df0df6c079ed5b8780
Andrew Jarrett <ahrjarrett@gmail.com>
2024-12-28 06:52:18 -0600

    📝(data): documents `integer.parse`

--- packages/data/src/integer.ts
+++ packages/data/src/integer.ts
@@ -1,5 +1,5 @@
 import { URI } from "@traversable/registry"
-import type { integer, number } from "@traversable/registry"
+import type { integer, newtype, number } from "@traversable/registry"
 import type { None, Option, Some } from "./exports.js"
 import type { numeric } from "./number.js"
 
@@ -7,6 +7,15 @@ import type { numeric } from "./number.js"
 const none = (): None => ({ _tag: URI.None })
 /** @internal */
 const some = <T>(value: T): Some<T> => ({ _tag: URI.Some, value })
+
+////////////////
+/// newtypes ///
+export interface NaN<_Min extends number = number, _Max extends number = number> extends newtype<number> {}
+export interface min<_ extends number = number> extends newtype<number> {}
+export interface max<_ extends number = number> extends newtype<number> {}
+export interface btwn<_Min extends number = number, _Max extends number = number> extends newtype<number> {}
+/// newtypes ///
+////////////////
 
 /**
  * ### {@link from `integer.from`}
@@ -32,36 +41,22 @@ export type from<T, N extends T extends number.integer ? T : never = T extends i
  */
 export type and<T, N extends number & T = number & T> = N
 
+
 export { integer_ as integer }
 function integer_(x: number): integer
 function integer_(x: number) { return globalThis.Math.round(x) }
 
+/**
+ * ### {@link is `integer.is`}
+ *
+ * A type guard that narrows its argument from `unknown` to {@link integer `integer`}
+ */
+export const is = (u: unknown): u is integer => typeof u === "number" && (+u.toFixed(0) === u)
+
 namespace integer_ {
-  /**
-   * ### {@link is `integer.is`}
-   *
-   * A type guard that narrows its argument from `unknown` to {@link integer `integer`}
-   */
-  export const is = (u: unknown): u is integer => typeof u === "number" && (+u.toFixed(0) === u)
-
   /** 
-   * -----------
-   * #### {@link parse `number.parse`} `(type)`
-   * 
-   * If {@link T `T`} is not parsable as an integer, returns `never`.
-   */
-  export type parseInt<T, Distributive = never> 
-    = [Distributive] extends [never] 
-    ? [T] extends [`${infer N extends number}.${string}$`] ? N
-    : [T] extends [`${infer N extends number}`] ? N 
-    : from<T>
-    : T extends T ? parseInt<T, never> : never
-
-  /** 
-   * ### {@link parse `number.parse`}
-   * 
-   * -----------
-   * #### {@link parse `number.parse`} `(term)`
+   * ### {@link parse `integer.parse`} `[term]`
+   * ---
    * 
    * If the 2nd argument (`safeParse`) is provided and {@link numeric `numeric`} 
    * parses as one of the following, returns {@link None `None`}:
@@ -71,6 +66,7 @@ namespace integer_ {
    * - {@link globalThis.Number.POSITIVE_INFINITY `Infinity`}
    * 
    * @example
+   *  /// [term]
    *  import { integer } from "@traversable/data"
    * 
    *  console.log(integer.parse("1"))   // 1
@@ -87,6 +83,66 @@ namespace integer_ {
       ? parsed 
       : globalThis.Number.isFinite(parsed) ? some(parsed) : none()
   }
+
+  /** 
+   * ### {@link parse `integer.parse`} `[type]`
+   * ---
+   * If {@link T `T`} is not parsable as an integer, returns `never`.
+   * 
+   * @example
+   *  /// [type]
+   *  import type { integer } from "@traversable/data"
+   * 
+   *  ///  example usage with numbers
+   *  type ex_01 = integer.parse<1>
+   *  //       ^?   type ex_01 = 1
+   *  type ex_02 = integer.parse<-2>
+   *  //       ^?   type ex_02 = -2
+   *  type ex_03 = integer.parse<3n>
+   *  //       ^?   type ex_03 = 3
+   *  type ex_04 = integer.parse<-4.4>
+   *  //       ^?   type ex_04 = -4
+   * 
+   *  ///  example usage with strings
+   *  type ex_11 = integer.parse<"11">
+   *  //       ^?    type ex_05 = 11
+   *  type ex_12 = integer.parse<"-12">
+   *  //       ^?    type ex_12 = -12
+   *  type ex_13 = integer.parse<"13n">
+   *  //       ^?    type ex_13 = 13
+   *  type ex_14 = integer.parse<"-14.14">
+   *  //       ^?    type ex_14 = -14
+   */
+  export type parse<T, U extends `${T & (string | number | bigint)}`= `${T & (string | number | bigint)}`>
+    = `${U}` extends `${infer N extends number}.${string}` ? N
+    : `${U}` extends `${infer N extends number}n` ? N
+    : `${U}` extends `${infer N extends number}` ? N
+    : from<T>
+    ;
+
+
+    /////////////////
+    ///  numbers  ///
+    type ex_01 = integer_.parse<1>
+    //   ^? type ex_01 = 1
+    type ex_02 = integer_.parse<-2>
+    //   ^? type ex_02 = -2
+    type ex_03 = integer_.parse<3n>
+    //   ^? type ex_03 = 3
+    type ex_04 = integer_.parse<-4.4>
+    //   ^? type ex_04 = -4
+    /////////////////
+    ///  strings  ///
+    type ex_11 = integer_.parse<"11">
+    //   ^? type ex_05 = 11
+    type ex_12 = integer_.parse<"-12">
+    //   ^? type ex_12 = -12
+    type ex_13 = integer_.parse<"13n">
+    //   ^? type ex_13 = 13
+    type ex_14 = integer_.parse<"-14.14">
+    //   ^? type ex_14 = -14
+
+
 
   export function isOdd(x: integer): boolean { return (+x & 1) === 1 }
   export function isEven(x: integer): boolean { return (+x & 1) === 0 }
