4a419826b9914fa0c86a687afd545e651b1e33be
Andrew Jarrett <ahrjarrett@gmail.com>
2025-02-10 00:08:16 -0600

    ✨(registry): checks-in the `tmp` dir

---
+++ packages/registry/src/tmp/node-weight-by-type.json.ts
@@ -0,0 +1,17 @@
+export type WeightByType = typeof WeightByType;
+export const WeightByType = {
+  "unknown": -1,
+  "null": 0,
+  "boolean": 10,
+  "integer": 20,
+  "number": 30,
+  "string": 40,
+  "const": 50,
+  "oneOf": 100,
+  "anyOf": 200,
+  "allOf": 300,
+  "record": 1000,
+  "object": 1100,
+  "tuple": 1200,
+  "array": 1400
+} as const;
